e182fea782dba0c2a3b46571c11534aff430e7b1
rohanmathewalex <rohanamthewalex66674@gmnail.com>
2020-04-19 15:12:24 +0530

    Load More

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,74 +1,95 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { Icon, Col, Card, Row } from "antd";
-import ImageSlider from '../../utils/ImageSlider';
+import ImageSlider from "../../utils/ImageSlider";
 
 const { Meta } = Card;
 
-
 function LandingPage() {
   const [Products, setProducts] = useState([]);
-  useEffect(() => {
-    Axios.post("/api/product/getProducts").then((response) => {
-      if (response.data.success) {
-        setProducts(response.data.products);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(8);
 
-        console.log(response.data.products);
-      } else {
-        alert("Failed to fetch product datas");
-      }
-    });
-  }, []);
+  useEffect(() => {
+    const variables = {
+      skip: Skip,
+      limit: Limit,
+    };
+
+    getProducts(variables)
+  }, [])
+
+  const getProducts = (variables) => {
+    Axios.post('/api/product/getProducts', variables)
+      .then(response => {
+        if (response.data.success) {
+
+          setProducts([...Products, ...response.data.products])
+
+          console.log(response.data.products)
+        } else {
+          alert('Failed to fetch product data')
+        }
+
+      })
+  }
+
+  //When clicking Load More button it loads more produts in Landing Page
+  const onLoadMore = () => {
+    let skip = Skip + Limit;
+
+    const variables = {
+      skip: skip,
+      limit: Limit,
+    };
+
+    getProducts(variables);
+  };
 
   const renderCards = Products.map((product, index) => {
-    return <Col lg={6} md={8} xs={24}>
-      <Card
-        hoverable={true}
-        cover={<ImageSlider images={product.images} />}
-      >
-
-        <Meta
-          title={product.title}
-          description={`$${product.price}`}
-        />
-
-
-
-      </Card>
-    </Col>
-  })
+    return (
+      <Col lg={6} md={8} xs={24}>
+        <Card hoverable={true} cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
+  });
   //template for landing Page
   return (
-    <div style={{ width: '75%', margin: '3rem auto' }}>
-      <div style={{ textAlign: 'center' }}>
-        <h2>Let's Travel Anywhere <Icon type='rocket' /></h2>
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travel Anywhere <Icon type="rocket" />
+        </h2>
       </div>
       {/* Filter */}
 
       {/*Search */}
 
       {Products.length === 0 ?
-        <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+        <div
+          style={{
+            display: "flex",
+            height: "300px",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <h2>No post yet ...!</h2>
         </div> :
+
         <div>
           {/*Creating Row for display In Responsive */}
-          <Row gutter={16, 16}>
-            {renderCards}
-
-          </Row>
-
+          <Row gutter={(16, 16)}>{renderCards}</Row>
         </div>
       }
       <br></br>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <button>Load More</button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button onClick={onLoadMore}>Load More</button>
       </div>
-
     </div>
-  )
+  );
 }
-
-
 
 export default LandingPage;
