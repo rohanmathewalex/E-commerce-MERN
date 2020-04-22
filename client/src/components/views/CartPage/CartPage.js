39af056adad9a969c19555c8a70a7ec15822e96d
rohanmathewalex <rohanamthewalex66674@gmnail.com>
2020-04-23 00:22:15 +0530

    product remove from cart feature completed

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { getCartItems } from '../../../_actions/user_actions';
+import { getCartItems, removeCartItem } from '../../../_actions/user_actions';
+import UserCardBlock from './Sections/UserCardBlock';
+import { Result, Empty } from 'antd';
+import Axios from 'axios';
 
 
 function CartPage(props) {
+    const [Total, setTotal] = useState(0)
 
     const dispatch = useDispatch();
+    const [ShowTotal, setShowTotal] = useState(false);
+    const [ShowSuccess, setShowSuccess] = useState(false)
 
     useEffect(() => {
 
@@ -16,16 +22,83 @@ function CartPage(props) {
                     cartItems.push(item.id)
                 });
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
+                    .then((response) => {
+                        if (response.payload.length > 0) {
+                            calculateTotal(response.payload)
+                        }
+
+                    })
             }
-
         }
-
 
     }, [props.user.userData])
 
+    //populating the  total calculation
+    const calculateTotal = (cartDetail) => {
+        let total = 0;
+
+        cartDetail.map(item => {
+            total += parseInt(item.price, 10) * item.quantity
+        });
+
+        setTotal(total)
+        setShowTotal(true)
+    }
+
+    //remove items from cart
+    const removeFromCart = (productId) => {
+        dispatch(removeCartItem(productId))
+            .then(() => {
+                Axios.get('/api/users/userCartInfo')
+                    .then(response => {
+                        if (response.data.success) {
+                            if (response.data.cartDetail.length <= 0) {
+                                setShowTotal(false)
+                            } else {
+                                calculateTotal(response.data.cartDetail)
+                            }
+
+                        } else {
+                            alert('Failed to get Cart Info')
+                        }
+                    })
+            })
+    }
+
+
     return (
-        <div>
-            CartPage
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <h1>My Cart</h1>
+            <div>
+                <UserCardBlock
+                    products={props.user.cartDetail}
+                    removeItem={removeFromCart}
+                />
+
+
+                {ShowTotal ?
+                    <div style={{ marginTop: '3rem' }}>
+                        <h2>Total amount: ${Total} </h2>
+                    </div>
+                    :
+                    ShowSuccess ?
+                        <Result
+                            status="success"
+                            title="Successfully Purchased Items"
+                        /> :
+                        <div style={{
+                            width: '100%', display: 'flex', flexDirection: 'column',
+                            justifyContent: 'center'
+                        }}>
+                            <br />
+                            <Empty description={false} />
+                            <p>No Items In the Cart</p>
+
+                        </div>
+                }
+
+            </div>
+
         </div>
     )
 }
